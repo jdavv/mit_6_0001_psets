cc37be31056b1e4c689601ec8afa6f75ef104998
duelus <davernjeanluc@gmail.com>
2018-04-25 21:38:10 -0500

    Solved ps1a for MIT6_0001F16

---
+++ ProblemSet1/ps1c.py
@@ -0,0 +1,9 @@
+# find the best rate of savings to achieve a down payment on $1M house in 36 months
+
+# Get user inputs
+annual_salary = float(120000)  # float(input('Enter your annual salary: '))
+total_cost = float(1000000)
+down_percentage = float(.25)
+semi_annual_raise = float(.07)
+r1 = float(.04)/12
+r = float(.04)

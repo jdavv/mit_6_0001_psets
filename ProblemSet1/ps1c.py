2121d1b00f1e0f545fda0d32007814a46db38d97
duelus <davernjeanluc@gmail.com>
2018-04-26 23:08:25 -0500

    Solved ps1a for MIT6_0001F16

--- ProblemSet1/ps1c.py
+++ ProblemSet1/ps1c.py
@@ -1,9 +1,28 @@
-# find the best rate of savings to achieve a down payment on $1M house in 36 months
+# # find the best rate of savings to achieve a down payment on $1M house in 36 months
+#
+# # Get user inputs
+# annual_salary = float(120000)  # float(input('Enter your annual salary: '))
+# total_cost = float(1000000)
+# down_percentage = float(.25)
+# semi_annual_raise = float(.07)
+# r1 = float(.04)/12
+# r = float(.04)
 
-# Get user inputs
-annual_salary = float(120000)  # float(input('Enter your annual salary: '))
-total_cost = float(1000000)
-down_percentage = float(.25)
-semi_annual_raise = float(.07)
-r1 = float(.04)/12
-r = float(.04)
+
+# Example from lecture #3 slides for bisection search
+
+# cube = 27
+# epsilon = 0.01
+# num_guesses = 0
+# low = 0
+# high = cube
+# guess = (high + low) / 2.0
+# while abs(guess**3 - cube) >= epsilon:
+#     if guess**3 < cube:
+#         low = guess
+#     else:
+#         high = guess
+#     guess = (high + low) / 2.0
+#     num_guesses +=1
+# print('num_guesses = ', num_guesses)
+# print(guess, 'is close to the cube root of', cube)

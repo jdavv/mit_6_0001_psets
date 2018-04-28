47324e16b5a5208ae84870d995e2cc2b878759cf
duelus <davernjeanluc@gmail.com>
2018-04-28 14:43:12 -0500

    Solved ps1a for MIT6_0001F16

--- ProblemSet1/ps1c.py
+++ ProblemSet1/ps1c.py
@@ -1,11 +1,12 @@
 # # find the best rate of savings to achieve a down payment on $1M house in 36 months
 
 # init salary variables
-start_annual_salary = float(120000)  # float(input('Enter your annual salary: '))
-r1 = float(.04)/12
+starting_annual_salary = float(input('Enter your annual salary: '))
 r = float(.04)
 semi_annual_raise = float(.07)
-monthly_salary = start_annual_salary / 12
+
+# init pay_in_three_years as True
+pay_in_three_years = True
 
 # init house variables
 total_cost = float(1000000)
@@ -15,34 +16,45 @@ total_down_payment = total_cost * down_percentage
 # init search variables
 epsilon = 100
 bisection_search_steps = 0
+
+# use integers as portion saved percentage
 max_portion_saved = 10000
 min_portion_saved = 0
+best_portion_saved_integer = max_portion_saved
 
 
 while True:
     bisection_search_steps += 1
+    annual_salary = starting_annual_salary
+    best_portion_saved = best_portion_saved_integer / 10000
+    monthly_saving = (annual_salary / 12) * best_portion_saved
     current_savings = 0
     months_of_saving = 0
-    current_savings += monthly_salary
-    current_savings += (current_savings * r / 12)
-    print(current_savings)
 
-# Example from lecture #3 slides for bisection search
+    # print(current_savings)
+    # print(best_portion_saved_integer)
+    while months_of_saving <= 36:
+        current_savings += monthly_saving + ((current_savings * r) / 12)
+        months_of_saving += 1
 
-# cube = 27
-# epsilon = 0.01
-# num_guesses = 0
-# low = 0
-# high = cube
-# guess = (high + low) / 2.0
-# while abs(guess**3 - cube) >= epsilon:
-#     if guess**3 < cube:
-#         low = guess
-#     else:
-#         high = guess
-#     guess = (high + low) / 2.0
-#     num_guesses +=1
-# print('num_guesses = ', num_guesses)
-# print(guess, 'is close to the cube root of', cube)
+        if months_of_saving % 6 == 0:
+            annual_salary += annual_salary * semi_annual_raise
+            monthly_saving = (annual_salary / 12) * best_portion_saved
 
+    if abs(current_savings - total_down_payment) <= epsilon:
+        break
 
+    if current_savings > total_down_payment:
+        max_portion_saved = best_portion_saved_integer
+    else:
+        min_portion_saved = best_portion_saved_integer
+
+    if min_portion_saved >= max_portion_saved:
+        pay_in_three_years = False
+        break
+    best_portion_saved_integer = (max_portion_saved + min_portion_saved) // 2
+if pay_in_three_years:
+    print('Best savings rate: ', best_portion_saved)
+    print('Steps in bisection search: ', bisection_search_steps)
+else:
+    print('It is not possible to pay the down payment in three years.')
